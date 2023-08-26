fc0c7822098221694165e7d87ebfa796a41c7b25
Raymond Lin <raymond.lin.7690@gmail.com>
2023-08-25 22:46:39 -0700

    Adding question class

--- question.rb
+++ question.rb
@@ -1,4 +1,25 @@
 class Question 
+  attr_accessor :number, :number2
 
-  
+  def initialize
+    @number = rand(1..20)
+    @number2 = rand(1..20)
+    @sum = number + number2
+  end
+
+  def ask_question
+    puts "Player: What is #{number} + #{number2} equal to?"
+  end
+
+  def answer(user_answer)
+    @sum == user_answer
+  end
 end
+
+def ask_question
+  question = Question.new
+  question.ask_question
+  user_answer = gets.chomp.to_i
+end
+
+#ask_question
